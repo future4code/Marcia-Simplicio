76d3c8f2c98929598124f3099a01fb3007973648
Marcia-Simplicio <smarciarj@outlook.com>
2020-09-28 18:43:37 -0300

    Semana3-aula9

---
+++ Semana3/Variaveis/index.js
@@ -0,0 +1,12 @@
+let nome = "Márcia";
+let idade = "38";
+console.log (mensagem);
+console.log (typeof nome);
+console.log (typeof idade);
+// está indefinido e é reconhecido como uma string
+let nome = "Márcia";
+let idade = "40";
+console.log (mensagem);
+console.log (typeof nome);
+console.log (typeof idade);
+// aparece string de novo
